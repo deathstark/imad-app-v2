22c59ef9a32689a06653311e83c3ed3a197018a7
deathstark <14it053@charusat.edu.in>
2017-03-15 17:50:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/hello', function (req, res) {
+  res.send(path.join('hello people'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
