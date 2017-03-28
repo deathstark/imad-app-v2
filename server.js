57fa7d5dec516cdc2542ee219ee66c69a2a2b432
deathstark <14it053@charusat.edu.in>
2017-03-28 12:33:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,84 +5,17 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
- var articles = {
-    'article-one':{
-        title:'Article One | sawan kumar',
-        heading:'Article One',
-        date:'24/09/1996',
-        content:`
-        <p>
-        Hi, This is my first article
-        </p>
-        <p>
-        Hi, This is my first article
-        </p>
-        <p>
-        Hi, This is my first article
-        </p>`
-        },
-    'article-two':{
-        title:'Article Two | sawan kumar',
-        heading:'Article One',
-        date:'25/09/1996',
-        content:`
-        <p>
-            Hi, This is my second article
-        </p>`}
 
-    
-};
-function createTemplate(data){
-    var title=data.title;
-    var date=data.date;
-    var heading=data.heading;
-    var content=data.content;
-    var htmlTemplate = `
-     <html>
-      <head>
-        <title>
-          ${title}
-        </title>
-        <meta name="viewport" content="width-device-width,initial-scale=1" />
-        <link href="/ui/style.css" rel="stylesheet" />
-        </head>
-    <body>
-        <div class="container">
-        <div>
-            <a href="/">Home</a>
-        </div>
-        <hr/>
-        <h3>
-           ${heading}
-        </h3>
-        <div>
-           ${date}
-        </div>
-        <div>
-           ${content}
-        </div>
-        </div>
-    </body>
-    
-</html>
-`;
-return htmlTemplate;
-}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleName', function (req, res) {
-   var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
+
+
+app.get('/CSS/elcomePage.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'welcomePage.css'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
