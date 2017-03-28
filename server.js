33ab3b7a846c0131c01b975ea6d371a424894716
deathstark <14it053@charusat.edu.in>
2017-03-28 12:41:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.get('/CSS/site.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'CSS', 'site.css'));
 });
 
+app.get('/Html/Signin.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'Html', 'Signin.html'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
